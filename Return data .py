56821b1c3665e1edcd122f5671de8a3571315e18
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-08-13 21:34:31 +0530

    Add files via upload

---
+++ Return data .py
@@ -0,0 +1,11 @@
+def student():
+    name = "Ahmed Raza"
+    Roll no = "289"
+    Cgpa = "9.17"
+
+    return name, Roll no, Cgpa
+
+name, Roll no, Cgpa = student()
+print(name)
+print(Roll no)
+print(Cgpa)
