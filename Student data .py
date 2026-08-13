ad4cf351e1bb361324b62e907a02894bf155aaa2
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-08-13 21:42:51 +0530

    Add files via upload

---
+++ Student data .py
@@ -0,0 +1,17 @@
+def student():
+    name = "Ahmed Raza"
+    roll_no = "289"
+    college = "SOCMACS"
+    year = "TY"
+    cgpa = "9.17"
+
+    return name, roll_no, college, year, cgpa
+
+
+name, roll_no, college, year, cgpa = student()
+
+print(name)
+print(roll_no)
+print(college)
+print(year)
+print(cgpa)
