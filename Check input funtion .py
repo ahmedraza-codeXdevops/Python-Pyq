dea8bfb389a27bbe43b4af1e3c6d2aa0b81cf65d
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-08-14 21:58:30 +0530

    Add files via upload

---
+++ Check input funtion .py
@@ -0,0 +1,11 @@
+a = (int(input("Enter a number:")))
+b = (int(input("Enter a number:")))
+c = (int(input("Enter a number:")))
+
+a = str("a")
+b = float(b)
+c = int(c)
+
+print("a is", a)
+print("b is", b)
+print("c is", c)
