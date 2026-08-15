b0025c11b5fa1903e22b7050084dac3248865150
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-08-15 12:50:46 +0530

    Add files via upload

---
+++ Inpur and output .py
@@ -0,0 +1,13 @@
+#name = int(input("Hello what is your name ?"))
+#print("Hello", name, "Welcome to the world of python programming")
+
+number = int(input("Enter a number: "))
+print("The number you entered is", number)
+
+age = int(input("Enter your age: "))
+if age>=18:
+    print("You are eligible to participate")
+else:
+    print("You are not eligible to participate")
+print("Your age is", age)
+
