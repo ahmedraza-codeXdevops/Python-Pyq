7f6e1d4a02da4b580a5eb7cc0c9705906bfe4054
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-08-15 12:49:59 +0530

    Add files via upload

---
+++ Datatype .py
@@ -0,0 +1,20 @@
+a = 12
+print(type(a))
+
+b = "7"
+print(type(b))
+
+c = 3.14
+print(type(c))
+
+name = input("Enter your name: ")
+print("type of name is", type(name))
+
+age = int(input("Enter your age: "))
+print("type of age is", type(age))
+
+price = float(input("Enter the price: "))
+print("type of price is", type(price))
+
+
+
