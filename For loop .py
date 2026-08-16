8bc93793f9fb759a2356b6d9a70c6c63532e5af6
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-08-16 19:14:56 +0530

    Add files via upload

---
+++ For loop .py
@@ -0,0 +1,36 @@
+'''for i in range (1 , 5):
+    print (i)'''
+
+# using range()
+'''for i in range(1, 7):
+    print(i)'''
+
+#using a string
+'''for char in "Hello":
+    print(char)'''
+
+#using a list
+'''fruits = ["apple", "banana", "mango"]
+
+for fruit in fruits:
+    print(fruit)'''
+
+#using a tuple
+'''numbers = (1, 2, 3, 4, 5)
+for n in numbers:
+    print(n)'''
+
+#using range(start, stop, step)
+'''for i in range(0, 10, 2):
+    print(i)'''
+
+#using a for loop with else
+'''for i in range(1, 5):
+    print(i)
+else:
+    print("Loop completed successfully.")'''
+
+#Nested loops
+for i in range(1, 4):
+    for j in range(1, 5):
+        print(i , j)
